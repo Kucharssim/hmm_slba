cd82282dd11e6db8529026b0cee867abb96ec113
Kucharssim <kucharssim@gmail.com>
2020-07-28 19:57:43 +0200

    improve plotting prior_pred_hmm_later

--- scripts/hmm_later.R
+++ scripts/hmm_later.R
@@ -8,26 +8,26 @@ hmm_later_prior_pred <- cmdstan_model(stan_file = here("stan", "later", "hmm_lat
 
 hyperparams <- list(
   # General info
-  N_obs = 200,
-  N_acc = 2,
-  N_sts = 2,
+  N_obs           = 200,
+  N_acc           = 2,
+  N_sts           = 2,
   
   # Design: 
-  nu_indices = matrix(1:4, nrow = 2, ncol = 2, byrow = TRUE),
-  sigma_indices = matrix(c(1,1, 2,2), nrow = 2, ncol = 2, byrow = TRUE),
-  alpha_indices = matrix(c(1,1, 2,2), nrow = 2, ncol = 2, byrow = TRUE),
-  t0_indices = rep(1, 2),
+  nu_indices      = matrix(1:4, nrow = 2, ncol = 2, byrow = TRUE),
+  sigma_indices   = matrix(c(1,1, 2,2), nrow = 2, ncol = 2, byrow = TRUE),
+  alpha_indices   = matrix(c(1,1, 2,2), nrow = 2, ncol = 2, byrow = TRUE),
+  t0_indices      = rep(1, 2),
   
   # Hyperparameters:
-  nu_alpha = c(8, 2, 5, 5), # correct state + guessing state
+  nu_alpha        = c(8, 2, 5, 5), # correct state + guessing state
   
-  sigma_mu = c(0.5, 0.5),
-  sigma_sigma = c(0.05, 0.05),
+  sigma_mu        = c(0.5, 0.5),
+  sigma_sigma     = c(0.05, 0.05),
   
-  alpha_mu = c(3, 1),
-  alpha_sigma = c(0.5, 0.5),
+  alpha_mu        = c(3, 1),
+  alpha_sigma     = c(0.5, 0.5),
   
-  t0_beta = as.array(1),
+  t0_beta         = as.array(1),
   
   init_prob_alpha = rep(5, 2),
   tran_prob_alpha = list(c(8, 2), c(2, 8))
@@ -39,9 +39,9 @@ generated_data$summary(variables = c("prop_state"))
 generated_data$summary(variables = c("prop_responses"))
 generated_data$summary(variables = c("mean_rt"))
 
-mcmc_hist(generated_data$draws(variables = "prop_state"))
-mcmc_hist(generated_data$draws(variables = "prop_responses"))
-mcmc_hist(generated_data$draws(variables = "mean_rt"))
+mcmc_hist(generated_data$draws(variables = "prop_state"),     facet_args = list(scales = "fixed", dir = "v"))
+mcmc_hist(generated_data$draws(variables = "prop_responses"), facet_args = list(scales = "fixed", dir = "v"))
+mcmc_hist(generated_data$draws(variables = "mean_rt"),        facet_args = list(scales = "fixed", dir = "v"))
 
 
 
