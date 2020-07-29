7f4b43f5540340f8693f55f201983285cb69c2f6
Kucharssim <kucharssim@gmail.com>
2020-07-29 21:08:49 +0200

    parameter recovery hmm_lba

--- scripts/hmm_later.R
+++ scripts/hmm_later.R
@@ -3,7 +3,11 @@ set_cmdstan_path("~/.cmdstan/cmdstan-2.24.0-rc1/")
 cmdstan_version()
 library(here)
 library(bayesplot)
+library(posterior)
+library(tidyverse)
 
+### Prior predictives ----
+n_predictive <- 250
 hmm_later_prior_pred <- cmdstan_model(stan_file = here("stan", "later", "hmm_later_prior_pred.stan"), include_paths = here()) 
 
 hyperparams <- list(
@@ -19,13 +23,13 @@ hyperparams <- list(
   t0_indices      = rep(1, 2),
   
   # Hyperparameters:
-  nu_alpha        = c(16, 4, 10, 10), # correct state + guessing state
+  nu_alpha        = c(14, 6, 10, 10), # correct state + guessing state
   
-  sigma_mu        = as.array(0.5),
-  sigma_sigma     = as.array(0.05),
+  sigma_mu        = as.array(0.4),
+  sigma_sigma     = as.array(0.1),
   
-  alpha_mu        = c(1, 0.5),
-  alpha_sigma     = c(0.2, 0.1),
+  alpha_mu        = c(0.5, 0.25),
+  alpha_sigma     = c(0.1, 0.05),
   
   t0_beta         = as.array(5),
   
@@ -33,7 +37,7 @@ hyperparams <- list(
   tran_prob_alpha = list(c(8, 2), c(2, 8))
 )
 
-generated_data <- hmm_later_prior_pred$sample(data = hyperparams, iter_warmup = 0, iter_sampling = 1000, fixed_param = TRUE)
+generated_data <- hmm_later_prior_pred$sample(data = hyperparams, iter_warmup = 0, iter_sampling = n_predictive, fixed_param = TRUE)
 
 generated_data$summary(variables = c("prop_state"))
 generated_data$summary(variables = c("prop_responses"))
@@ -44,7 +48,96 @@ mcmc_hist(generated_data$draws(variables = "prop_responses"), facet_args = list(
 mcmc_hist(generated_data$draws(variables = "mean_rt"),        facet_args = list(scales = "fixed", dir = "v"))
 
 
-hist(as.vector(generated_data$draws(variables = "rt")[5,1,]), breaks=20)
+hist(as.vector(generated_data$draws(variables = "rt")[10,1,]), breaks=20)
+
+rt <- generated_data$draws("rt")
+responses <- generated_data$draws("responses")
+state <- generated_data$draws("state")
+
+### Parameter recovery ----
+parameters <- c("nu_vec", "sigma", "alpha", "t0", "init_prob[1]", "tran_prob[1,1]", "tran_prob[2,2]")
+generating_parameters <- as_tibble(posterior::as_draws_df(generated_data$draws(parameters)))
+hmm_later <- cmdstan_model(stan_file = here("stan", "later", "hmm_later.stan"), include_paths = here())
+
+initFun <- function(){
+  prior <- hmm_later_prior_pred$sample(data = hyperparams, iter_warmup = 0, iter_sampling = 1, fixed_param = TRUE)
+  out <- list(
+    #init_prob = as.vector(prior$draws(variables = "init_prob")),
+    
+    #nu = round(as.vector(prior$draws(variables = "nu")), 2),
+    sigma = as.vector(prior$draws(variables = "sigma")),
+    alpha = as.vector(prior$draws(variables = "alpha")),
+    t0 = as.vector(prior$draws(variables = "t0"))
+    )
+  
+  out 
+}
+
+# https://stackoverflow.com/questions/34208564/how-to-hide-or-disable-in-function-printed-message/34208658#34208658
+# hide output from Stan
+quiet <- function(x) { 
+  sink(tempfile()) 
+  on.exit(sink()) 
+  invisible(force(x)) 
+} 
+
+map <- function(stan_data, true_states, max_iter = 50){
+  converged <- FALSE
+  i <- 1
+  while(i <= max_iter && !converged){
+    optims <- suppressWarnings(quiet(hmm_later$optimize(data = stan_data, init = initFun, refresh = 0)))
+    converged <- length(optims$output_files(include_failed = FALSE)) != 0  
+    if(converged){
+      # assess label switching
+      est_states <- apply(matrix(optims$mle("state_prob"), ncol = 2, byrow = TRUE), 1, which.max)
+      p_agree <- mean(est_states == true_states)
+      if(p_agree < 0.5) converged <- FALSE
+    }
+    i <- i + 1
+  }
+  
+  if(converged){
+    return(optims$mle(variables = parameters))
+  } else{
+    nas <- rep(NA, ncol(estimated_parameters))
+    names(nas) <- colnames(estimated_parameters)
+    return(nas)
+  }
+}
+
+stan_data <- hyperparams
+
+estimated_parameters <- dplyr::select(generating_parameters, -c(".chain", ".iteration", ".draw"))
+pb <- dplyr::progress_estimated(n = n_predictive)
+for(i in seq_len(n_predictive)){
+  stan_data$rt <- as.vector(rt[i,1,])
+  stan_data$responses <- as.vector(responses[i,1,])
+  estimated_parameters[i,] <- as.list(map(stan_data, as.vector(state[i,1,])))
+  pb$tick()$print()
+}
+
+parameters_names <- names(estimated_parameters)
+mean(complete.cases(estimated_parameters))
+kk <- sqrt(length(parameters_names))
+par(mfrow = c(floor(kk), ceiling(kk)))
+for(p in parameters_names){
+  x <- generating_parameters[,p,drop=TRUE]
+  y <- estimated_parameters[,p,drop=TRUE]
+  plot(x, y, pch = 19, xlab = "True", ylab = "Estimated", main = p, 
+       xlim = range(c(x, y), na.rm = TRUE), 
+       ylim = range(c(x, y), na.rm = TRUE))
+  abline(0, 1)
+  rug(generating_parameters[,p,drop=TRUE], col = (!complete.cases(estimated_parameters)) + 1)
+}
+par(mfrow = c(1, 1))
+
+# stan_data <- hyperparams
+# stan_data$N_obs <- nrow(tdA)
+# stan_data$rt <- tdA$RT/1000
+# stan_data$responses <- ifelse(tdA$correct == 1, 1, 2) 
+# samples <- hmm_later$sample(data = stan_data, chains = 16, parallel_chains = 4, refresh = 500, init = initFun)
+# optims  <- hmm_later$optimize(data = stan_data, init = initFun)
+# 
 # library(rstan)
 # foo <- stan_model(file = here("stan", "later", "hmm_later_prior_pred.stan"))
 # dat <- sampling(foo, hyperparams, algorithm = "Fixed_param", chains = 1)
