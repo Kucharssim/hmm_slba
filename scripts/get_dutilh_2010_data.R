a5f6b8970224275204471b7a551db381535237a6
Kucharssim <kucharssim@gmail.com>
2020-08-03 16:23:54 +0200

    correct dutilh data

--- scripts/get_dutilh_2010_data.R
+++ scripts/get_dutilh_2010_data.R
@@ -4,10 +4,10 @@ load(url("http://www.gillesdutilh.com/materials/PTM/PTMdat.Rdata"))
 
 table(experiment = td$experiment, subject = td$subject)
 
-for(subject in unique(td$subject)){
-  sub <- subset(td, subject == subject)
+for(s in unique(td$subject)){
+  sub <- subset(td, subject == s)
   sub$rt <- sub$RT / 1000
   sub$accumulator <- ifelse(sub$correct == 1, 1, 2)
   sub <- subset(sub, select = c("pacc", "rt", "accumulator"))
-  readr::write_csv(x = sub, path = here("data", sprintf("dutilh_2010_subject_%s.csv", subject)))
+  readr::write_csv(x = sub, path = here("data", sprintf("dutilh_2010_subject_%s.csv", s)))
 }
