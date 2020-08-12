914611697a43e0a8326dcb209757fecdd7c0b262
Kucharssim <kucharssim@gmail.com>
2020-08-12 15:10:21 +0200

    fit hmm_later

--- scripts/fit_hmm_later.R
+++ scripts/fit_hmm_later.R
@@ -19,12 +19,17 @@ stan_data$rt <- data$rt
 stan_data$responses <- data$accumulator
 
 initFun <- function(){
-  prior <- hmm_later_prior_pred$sample(data = hyperparams, iter_warmup = 0, iter_sampling = 1, fixed_param = TRUE)
-  out <- list(
-    sigma = as.vector(prior$draws(variables = "sigma")),
-    alpha = as.vector(prior$draws(variables = "alpha")),
-    t0 = as.vector(prior$draws(variables = "t0"))
-  )
+  valid <- FALSE
+  while(!valid){
+    prior <- hmm_later_prior_pred$sample(data = hyperparams, iter_warmup = 0, iter_sampling = 1, fixed_param = TRUE)
+    out <- list(
+      sigma = as.vector(prior$draws(variables = "sigma")),
+      alpha = as.vector(prior$draws(variables = "alpha")),
+      t0 = as.vector(prior$draws(variables = "t0")),
+      nu = c(0.6, 0.4, 0.5, 0.5)/2
+    )
+    valid <- out$alpha[1] > out$alpha[2]
+  }
   
   out 
 }
@@ -32,7 +37,7 @@ initFun <- function(){
 #### Get MCMC samples ----
 samples <- hmm_later$sample(data = stan_data, 
                             chains = 8, parallel_chains = 4, iter_warmup = 1000, iter_sampling = 1000,
-                            init = initFun, adapt_delta = 0.99)
+                            init = initFun, adapt_delta = 0.9)
 samples$summary(variables = c("nu_vec[1,1]", "nu_vec[2,1]", "alpha", "sigma", "t0"))
 
 samples$save_object(file = here("saves", "fit_hmm_later", sprintf("dutilh_2010_subject_%s.Rds", subject)))
