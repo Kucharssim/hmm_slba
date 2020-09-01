56681dc99454763d8c008b58e767cbc9faf6667e
Kucharssim <kucharssim@gmail.com>
2020-09-01 23:14:56 +0200

    added prior pred plots

--- scripts/hmm_later.R
+++ scripts/hmm_later.R
@@ -51,8 +51,6 @@ generated_data <- readRDS(here("saves", "prior_predictives.Rds"))
 # mcmc_hist(generated_data$draws(variables = "mean_rt"),        facet_args = list(scales = "fixed", dir = "v"))
 
 
-# hist(as.vector(generated_data$draws(variables = "rt")[10,1,]), breaks=20)
-
 rt <- generated_data$draws("rt")
 responses <- generated_data$draws("responses")
 state <- generated_data$draws("state")
@@ -62,6 +60,93 @@ run_lengths <- sapply(1:dim(state)[1], function(x) mean(rle(as.vector(state[x,,]
 summary(run_lengths)
 sd(run_lengths)
 #
+
+rt_df <- as_tibble(as_draws_df(rt)) %>%
+  pivot_longer(cols = starts_with("rt"), names_to = "trial", values_to = "rt", names_pattern = "rt\\[(.*)\\]")
+
+responses_df <- as_tibble(as_draws_df(responses)) %>%
+  pivot_longer(cols = starts_with("responses"), names_to = "trial", values_to = "response", names_pattern = "responses\\[(.*)\\]")
+
+state_df <- as_tibble(as_draws_df(state)) %>%
+  pivot_longer(cols = starts_with("state"), names_to = "trial", values_to = "state", names_pattern = "state\\[(.*)\\]")
+
+# generate descriptives of the prior predictive response times, under the two states, and for the two alternatives
+prior_pred_df <- left_join(rt_df, responses_df) %>% left_join(state_df) %>%
+  mutate(state_text = ifelse(state == 1, "Controlled", "Guessing"),
+         response_text = ifelse(response == 1, "Correct", "Error")) %>%
+  mutate(comb_state_response = sprintf("State: %s, Response: %s", state_text, response_text))
+
+prior_pred_df %>% group_by(state_text, response_text) %>% summarise(long_rt = mean(rt > 3))
+prior_pred_df %>% group_by(.iteration) %>% summarise(long_rt = mean(rt > 3)) %>% ungroup() %>% summary()
+
+# descriptives: RT per state and response
+prior_pred_df %>%
+  group_by(.draw, state_text, response_text) %>%
+  summarise(mean_rt = mean(rt)) %>%
+  ungroup() %>%
+  group_by(state_text, response_text) %>%
+  summarise(mean  = mean(mean_rt), 
+            sd    = sd(mean_rt),
+            lower = quantile(mean_rt, 0.025),
+            lqrt  = quantile(mean_rt, 0.25),
+            median= quantile(mean_rt, 0.5),
+            uqrt  = quantile(mean_rt, 0.75),
+            upper = quantile(mean_rt, 0.975)) %>%
+  knitr::kable(digits = 2)
+  # knitr::kable(digits = 2, format = "latex", booktabs = TRUE)
+
+prior_pred_df %>%
+  group_by(.draw, comb_state_response) %>%
+  summarise(rt = mean(rt)) %>%
+  ungroup() %>%
+  ggplot(aes(x = rt)) + 
+  geom_histogram(bins = 50) +
+  facet_wrap(~comb_state_response, scales = "free_y") + 
+  ylab("Count") + xlab("Average response time (sec)") +
+  theme_bw(base_size = 15) + 
+  theme(aspect.ratio = 0.7, 
+        panel.grid.minor = element_blank(), panel.grid.major = element_blank())
+ggsave(filename = here("figures", "simulations", "prior_pred_mean_rt.png"), width = 20, height = 15, unit = "cm")
+
+prior_pred_df %>% 
+  subset(.draw < 10 & rt < 3) %>%
+  ggplot(aes(x = rt, group = .draw)) + 
+  geom_density() + 
+  facet_wrap(~comb_state_response, scales = "free_y") + 
+  ylab("Density") + xlab("Response time (sec)") +
+  theme_bw(base_size = 15) + 
+  theme(aspect.ratio = 0.7, 
+        panel.grid.minor = element_blank(), panel.grid.major = element_blank())
+ggsave(filename = here("figures", "simulations", "prior_pred_rt.png"), width = 20, height = 15, unit = "cm")
+
+# descriptives: responses per state
+prior_pred_df %>%
+  group_by(.draw, state_text) %>%
+  summarise(prop_correct = mean(response == 1), n_correct = sum(response == 1)) %>%
+  ungroup() %>%
+  group_by(state_text) %>% 
+  summarise(mean  = mean(prop_correct), 
+            sd    = sd(prop_correct),
+            lower = quantile(prop_correct, 0.025),
+            lqrt  = quantile(prop_correct, 0.25),
+            median= quantile(prop_correct, 0.5),
+            uqrt  = quantile(prop_correct, 0.75),
+            upper = quantile(prop_correct, 0.975)) %>%
+  knitr::kable(digits = 2)
+  # knitr::kable(digits = 2, format = "latex", booktabs = TRUE)
+
+prior_pred_df %>% 
+  group_by(.draw, state_text) %>%
+  summarise(prop_correct = mean(response == 1)) %>%
+  ggplot(aes(x = prop_correct)) +
+  geom_histogram(bins = 50) +
+  facet_wrap(~state_text) + 
+  ylab("Count") + xlab("Proportion of correct responses") +
+  theme_bw(base_size = 15) + 
+  theme(aspect.ratio = 0.7, 
+        panel.grid.minor = element_blank(), panel.grid.major = element_blank())
+ggsave(filename = here("figures", "simulations", "prior_pred_accuracy.png"), width = 20, height = 8, unit = "cm")
+  
 
 ### Parameter recovery ----
 parameters <- c("nu_vec[1,1]", "nu_vec[2,1]", "sigma", "alpha", "t0", "init_prob[1]", "tran_prob[1,1]", "tran_prob[2,2]")
