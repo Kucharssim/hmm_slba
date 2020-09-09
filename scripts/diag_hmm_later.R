9a1f6303d7f8bb5f667aaf5c68f68bbd72eb1e5e
Kucharssim <kucharssim@gmail.com>
2020-09-09 19:46:21 +0200

    add pdf fit plots

--- scripts/diag_hmm_later.R
+++ scripts/diag_hmm_later.R
@@ -5,6 +5,7 @@ library(here)
 library(bayesplot)
 library(posterior)
 library(tidyverse)
+library(patchwork)
 
 source(here("R", "expose_stan_functions.R"))
 
@@ -34,7 +35,8 @@ for(subject in LETTERS[1:11]){
   
   traceplot <- bayesplot::mcmc_trace(samples$draws(variables = parameters), facet_args = list(ncol = 3)) +
     ggplot2::ggtitle(sprintf("Subject %s", subject))
-  ggplot2::ggsave(filename = here("figures", "traceplots", sprintf("dutilh_2010_subject_%s.png", subject)))
+  ggplot2::ggsave(filename = here("figures", "traceplots", sprintf("dutilh_2010_subject_%s.png", subject)), 
+                  width = 15, height = 15, units = "cm")
   
   # reshape state probability draws into a long data frame
   state_prob <- samples$draws("state_prob") %>% 
@@ -45,10 +47,38 @@ for(subject in LETTERS[1:11]){
                  values_to     = "probability")  %>%
     mutate(state = as.integer(state), obs = as.integer(obs))
   
+  # descriptive plot
+  state_prob_summary <- state_prob %>% 
+    subset(state == 1) %>% 
+    group_by(obs) %>% 
+    summarise(mean = mean(probability), lower = quantile(probability, 0.05), upper = quantile(probability, 0.95))
+  desc_plot <- list()
+  desc_plot[['response']] <- ggplot(data, aes(x = obs, y = accumulator)) + 
+    geom_line(data = data %>% mutate(ma = forecast::ma(accumulator, 5)), mapping = aes(x = obs, y = ma)) + 
+    geom_point() + 
+    xlab("") + ylab("Response") +
+    scale_y_continuous(breaks = 1:2, labels = c("Incorrect", "Correct"), expand = c(0.2,0)) +
+    theme_light()
+  desc_plot[['rt']] <- ggplot(data, aes(x = obs, y = rt)) + 
+    geom_line() +
+    xlab("") + ylab("Response time (sec)") +
+    scale_y_continuous(limits = c(0, NA)) +
+    theme_light()
+  desc_plot[['state']] <- ggplot(state_prob_summary, aes(x = obs, y = mean, ymin = lower, ymax = upper)) + 
+    geom_line() + 
+    geom_ribbon() +
+    xlab("Trial") + ylab("P(controlled state)") + 
+    theme_light()
+  desc_plot[['response']] + desc_plot[['rt']] + desc_plot[['state']] + patchwork::plot_layout(nrow = 3) +
+    patchwork::plot_annotation(title = sprintf("Participant %s", subject)) &
+    theme(text = element_text(size = 18), legend.title = element_blank(), legend.position = "none")
+  ggplot2::ggsave(filename = here("figures", "desc_plots", sprintf("dutilh_2010_subject_%s.png", subject)), 
+                  width = 10, height = 20, units = "cm")
+  
   random_draws <- sample(unique(state_prob$.draw), size = 1000, replace = FALSE)
   
   # plot empirical cumulative rt distributions scaled by proportions of correct/incorrect and state1/state2
-  cum_plot <- state_prob %>%  
+  cum_plot <- state_prob %>%
     # subset only a portion of draws
     subset(.draw %in% random_draws) %>%
     left_join(data) %>%
@@ -58,19 +88,19 @@ for(subject in LETTERS[1:11]){
     ungroup() %>%
     group_by(state, accumulator, rt) %>%
     summarise(median = median(cum_prob), lower = quantile(cum_prob, 0.05), upper = quantile(cum_prob, 0.95)) %>%
-    mutate(group = sprintf("%s state, %s response", 
+    mutate(group = sprintf("%s state, %s response",
                            ifelse(state==1, "accurate", "fast"),
                            ifelse(accumulator==1, "correct", "incorrect")
                            )
            ) %>%
-    ggplot(aes(x = rt, y = median, group = group, fill = group)) + 
-    geom_line() + 
+    ggplot(aes(x = rt, y = median, group = group, fill = group)) +
+    geom_line() +
     geom_ribbon(aes(ymin=lower,ymax=upper), alpha = 0.5) +
-    scale_fill_manual(values = c("darkred", "red", "darkblue", "blue")) + 
+    scale_fill_manual(values = c("darkred", "red", "darkblue", "blue")) +
     ylab("Cumulative probability") +
-    xlab("Response time") + 
-    ggtitle(sprintf("Subject %s", subject)) + 
-    theme_light() + 
+    xlab("Response time (sec)") +
+    ggtitle(sprintf("Subject %s", subject)) +
+    theme_light() +
     theme(text = element_text(size = 20), legend.title = element_blank(), legend.position = "none")
   
   # get samples of parameters
@@ -79,15 +109,15 @@ for(subject in LETTERS[1:11]){
     as_tibble() %>%
     pivot_longer(cols = starts_with(parameters), names_pattern = "(.*)\\[(.*)\\]", names_to = c("parameter", "state")) %>%
     subset(.draw %in% random_draws) %>%
-    mutate(parameter = gsub("_vec", "_acc", parameter), state = as.integer(gsub(",1", "", state)))
+    mutate(parameter = gsub("_vec", "_acc", parameter), state = as.integer(gsub(",(1|2)", "", state)))
   
   
-  # plot model based cumulative rt distributions
+  # # plot model based cumulative rt distributions
   df <- expand.grid(
     rt          = seq(0, max(data$rt), length.out = 20),
     accumulator = 1:2,
     state       = 1:2,
-    .draw       = random_draws[1:400],
+    .draw       = random_draws[1:100],
     KEEP.OUT.ATTRS = FALSE, stringsAsFactors = FALSE)
 
   # compute the cumulative rt distributions
@@ -100,14 +130,14 @@ for(subject in LETTERS[1:11]){
     alpha <- subset(samples_params, .draw == draw_ & parameter == "alpha" & state == state_)$value
     alpha <- rep(alpha, 2)
     t0    <- subset(samples_params, .draw == draw_ & parameter == "t0")$value
-    
+
     # scale by the probability of response and state
     n_cases <- sum(subset(state_prob, .draw == draw_ & state == state_)$probability)
-    if(accumulator == 2) n_cases <- nrow(data) - n_cases
+    # if(accumulator == 2) n_cases <- nrow(data) - n_cases
     plater(rt, accumulator, nu, sigma, alpha, t0) * n_cases / nrow(data)
   }
-  
-  df <- df %>% 
+
+  df <- df %>%
     group_by(accumulator, state, .draw) %>%
     # compute the cdf
     mutate(cum_prob = call_plater(rt, unique(accumulator), unique(.draw), unique(state))) %>%
@@ -117,17 +147,89 @@ for(subject in LETTERS[1:11]){
     summarise(median = median(cum_prob), lower = quantile(cum_prob, 0.05), upper = quantile(cum_prob, 0.95)) %>%
     ungroup() %>%
     # labeling should be the same as for the data plot
-    mutate(group = sprintf("%s state, %s response", 
+    mutate(group = sprintf("%s state, %s response",
                            ifelse(state==1, "accurate", "fast"),
                            ifelse(accumulator==1, "correct", "incorrect")
                            )
            )
-    
-  cum_plot +
-    geom_line(data = df, linetype = 2) + 
-    geom_ribbon(aes(ymin=lower,ymax=upper), alpha = 0.25, data = df)
-  ggplot2::ggsave(filename = here("figures", "cum_plots", sprintf("dutilh_2010_subject_%s.png", subject)))
 
+  cum_plot <- cum_plot +
+    geom_line(data = df, linetype = 2) +
+    geom_ribbon(aes(ymin=lower,ymax=upper), alpha = 0.25, data = df)
+  ggplot2::ggsave(plot = cum_plot, filename = here("figures", "cum_plots", sprintf("dutilh_2010_subject_%s.png", subject)),
+                  width = 15, height = 15, units = "cm")
+  
+  pdf_plot <- data %>% 
+    mutate(response = ifelse(accumulator == 1, "Correct", "Incorrect")) %>%
+    ggplot(aes(x = rt)) + 
+      geom_histogram(bins = 25, fill = "grey", col = "black") + 
+      facet_wrap(~response)
+  
+  df <- expand.grid(
+    rt          = seq(0, max(data$rt), length.out = 100),
+    accumulator = 1:2,
+    state       = 1:2,
+    .draw       = random_draws[1:100],
+    KEEP.OUT.ATTRS = FALSE, stringsAsFactors = FALSE)
+  
+  # compute the pdf for each state
+  call_dlater <- function(rt, accumulator, draw_, state_){
+    #browser()
+    nu <- subset(samples_params, .draw == draw_ & parameter == "nu_acc" & state == state_)$value
+    nu <- c(nu, 1-nu)
+    sigma <- subset(samples_params, .draw == draw_ & parameter == "sigma")$value
+    sigma <- rep(sigma, 2)
+    alpha <- subset(samples_params, .draw == draw_ & parameter == "alpha" & state == state_)$value
+    alpha <- rep(alpha, 2)
+    t0    <- subset(samples_params, .draw == draw_ & parameter == "t0")$value
+    # scale by the probability of response and state
+    n_cases <- sum(subset(state_prob, .draw == draw_ & state == state_)$probability)
+    #if(accumulator == 2) n_cases <- nrow(data) - n_cases
+    dlater(rt, accumulator, nu, sigma, alpha, t0) * n_cases / nrow(data)
+  }
+  
+  df <- df %>%
+    group_by(accumulator, state, .draw) %>%
+    # compute the pdf
+    mutate(pdf = call_dlater(rt, unique(accumulator), unique(.draw), unique(state))) %>%
+    ungroup() %>%
+    mutate(accumulator = ifelse(accumulator == 1, "Correct", "Incorrect"))
+  df_by_state <- df %>%  
+    # compute confidence bounds + median
+    group_by(state, accumulator, rt) %>%
+    summarise(median = median(pdf), lower = quantile(pdf, 0.05), upper = quantile(pdf, 0.95)) %>%
+    ungroup()
+  df_aggregate <- df %>% 
+    pivot_wider(id_cols = c("rt", "accumulator", ".draw"), names_from = state, values_from = pdf) %>%
+    mutate(pdf = `1` + `2`) %>%
+    group_by(accumulator, rt) %>%
+    summarise(median = median(pdf), lower = quantile(pdf, 0.05), upper = quantile(pdf, 0.95))
+  
+  pdf_plot <- data %>% 
+    mutate(prop = ifelse(accumulator == 1, mean(data$accumulator == 1), mean(data$accumulator == 2))) %>%
+    mutate(cut = cut(rt, breaks = seq(0, max(rt), length.out = 50))) %>%
+    group_by(cut, accumulator) %>%
+    summarise(n = n()) %>%
+    ungroup() %>%
+    mutate(prop = ifelse(accumulator == 1, n / sum(accumulator == 1), n / sum(accumulator == 2))) %>%
+    mutate(cut = gsub("\\(|\\]", "", cut)) %>%
+    separate(cut, c("min", "max"), sep = ",", convert = TRUE) %>%
+    mutate(x = (min + max) /2, width = max - min) %>%
+    mutate(prop = (prop / sum(prop)) / width) %>%
+    mutate(accumulator = ifelse(accumulator == 1, "Correct", "Incorrect")) %>%
+    ggplot(aes(x=x, y=prop)) + geom_bar(stat="identity", fill = "grey", col = "black", alpha = 0.3) + 
+      geom_ribbon(data = df_by_state, mapping  = aes(x = rt, ymin = lower, ymax = upper, group = state, fill = as.factor(state)), alpha = 0.5, inherit.aes = FALSE) + 
+      geom_line(data   = df_by_state, mapping  = aes(x = rt, y = median, group = state), alpha = 0.5, inherit.aes = FALSE) + 
+      geom_ribbon(data = df_aggregate, mapping = aes(x = rt, ymin = lower, ymax = upper), fill = "black", alpha = 0.7, inherit.aes = FALSE) +
+      geom_line(data   = df_aggregate, mapping = aes(x = rt, y = median), inherit.aes = FALSE) +
+      xlab("Response time (sec)") + ylab("Density") +
+      facet_wrap(~accumulator) + 
+      ggtitle(sprintf("Subject %s", subject)) +
+      theme_light() +
+      theme(text = element_text(size = 20), legend.title = element_blank(), legend.position = "none")
+  ggplot2::ggsave(plot = pdf_plot, filename = here("figures", "pdf_plots", sprintf("dutilh_2010_subject_%s.png", subject)),
+                  width = 25, height = 15, units = "cm")
+  
 }
 
 
